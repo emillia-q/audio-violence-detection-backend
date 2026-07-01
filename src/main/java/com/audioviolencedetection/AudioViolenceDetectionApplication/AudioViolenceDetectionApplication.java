56ae55f64eea85eba85bd4fb25bca5bf4b31a044
Emilia Kura <emillia.kura@gmail.com>
2026-07-01 11:03:48 +0200

    initial commit

---
+++ src/main/java/com/audioviolencedetection/AudioViolenceDetectionApplication/AudioViolenceDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.audioviolencedetection.AudioViolenceDetectionApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioViolenceDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioViolenceDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/audioviolencedetection/AudioViolenceDetectionApplication/AudioViolenceDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audioviolencedetection.AudioViolenceDetectionApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioViolenceDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
