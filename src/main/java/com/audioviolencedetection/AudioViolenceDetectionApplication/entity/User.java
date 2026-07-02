b44559e4769c0c127aa65f309dfbbf3bbf8d5f44
Emilia Kura <emillia.kura@gmail.com>
2026-07-02 16:56:56 +0200

    feat: create user class

---
+++ src/main/java/com/audioviolencedetection/AudioViolenceDetectionApplication/entity/User.java
@@ -0,0 +1,28 @@
+package com.audioviolencedetection.AudioViolenceDetectionApplication.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", length = 254, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", length = 60, nullable = false)
+    private String password;
+
+    @OneToOne
+    @JoinColumn(name = "trusted_user_id", referencedColumnName = "id")
+    private User trustedUser;
+}
