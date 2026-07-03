1d45043664c69a9821b6f698f789f27a38f07d34
Emilia Kura <emillia.kura@gmail.com>
2026-07-03 11:20:48 +0200

    feat: create device class

---
+++ src/main/java/com/audioviolencedetection/AudioViolenceDetectionApplication/entity/Device.java
@@ -0,0 +1,31 @@
+package com.audioviolencedetection.AudioViolenceDetectionApplication.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "devices")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "mac_address", length = 17, nullable = false, unique = true)
+    private String macAddress;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "is_activated", nullable = false)
+    private Boolean isActivated = false;
+}
