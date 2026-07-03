6e6bc970c24542e666efb5137cd5d1b17cb5efb5
Emilia Kura <emillia.kura@gmail.com>
2026-07-03 15:35:59 +0200

    feat: implement CustomUserDetailsService

---
+++ src/main/java/com/audioviolencedetection/api/security/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.audioviolencedetection.api.security.service;
+
+import com.audioviolencedetection.api.repository.UserRepository;
+import com.audioviolencedetection.api.security.model.SecurityUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(SecurityUser::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email: " + email + " not found"));
+    }
+}
