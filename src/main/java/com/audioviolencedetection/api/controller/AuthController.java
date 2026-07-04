30a68b9e6496e2c04964a443e89b0a73f0dd9f96
Emilia Kura <emillia.kura@gmail.com>
2026-07-04 17:45:57 +0200

    feat: create AuthController

---
+++ src/main/java/com/audioviolencedetection/api/controller/AuthController.java
@@ -0,0 +1,9 @@
+package com.audioviolencedetection.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+public class AuthController {
+}
