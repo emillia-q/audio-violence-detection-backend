80db60f13134cc8e655172be48a61657210a9835
Emilia Kura <emillia.kura@gmail.com>
2026-07-04 20:05:28 +0200

    feat: add OpenApiConfig for Swagger JWT authentication

---
+++ src/main/java/com/audioviolencedetection/api/security/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.audioviolencedetection.api.security.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Audio Violence Detection API",
+                version = "1.0",
+                description = "API for audio violence detection system"
+        )
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {
+}
