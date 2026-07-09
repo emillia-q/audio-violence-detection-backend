f57608a21e95753fff4b05816f446633f9a09a69
Emilia Kura <emillia.kura@gmail.com>
2026-07-09 14:16:45 +0200

    feat: add swagger annotations in setTrustedUser method in UserController

--- src/main/java/com/audioviolencedetection/api/controller/UserController.java
+++ src/main/java/com/audioviolencedetection/api/controller/UserController.java
@@ -3,6 +3,8 @@ package com.audioviolencedetection.api.controller;
 import com.audioviolencedetection.api.dto.request.AddTrustedUserRequest;
 import com.audioviolencedetection.api.security.model.SecurityUser;
 import com.audioviolencedetection.api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +22,10 @@ public class UserController {
 
     @PatchMapping("/trusted-user")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Set trusted user")
+    @ApiResponse(responseCode = "204", description = "Trusted user set")
+    @ApiResponse(responseCode = "400", description = "Invalid request data or validation failed")
+    @ApiResponse(responseCode = "404", description = "User not found")
     public void setTrustedUser(@Valid @RequestBody AddTrustedUserRequest request,
                                @AuthenticationPrincipal SecurityUser securityUser) {
         userService.setTrustedUser(request, securityUser.getId());
