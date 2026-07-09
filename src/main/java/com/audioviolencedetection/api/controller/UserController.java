bc6bc38117bdd6c7affe10d21934330719686326
Emilia Kura <emillia.kura@gmail.com>
2026-07-09 14:13:55 +0200

    feat: add SecurityRequirement annotation to UserController
    - For swagger to properly pass the token.

--- src/main/java/com/audioviolencedetection/api/controller/UserController.java
+++ src/main/java/com/audioviolencedetection/api/controller/UserController.java
@@ -3,6 +3,7 @@ package com.audioviolencedetection.api.controller;
 import com.audioviolencedetection.api.dto.request.AddTrustedUserRequest;
 import com.audioviolencedetection.api.security.model.SecurityUser;
 import com.audioviolencedetection.api.service.UserService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path = "/api/v1/users")
 @RequiredArgsConstructor
+@SecurityRequirement(name = "Bearer Authentication")
 public class UserController {
 
     private final UserService userService;
