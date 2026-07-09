2a48d88af2798ab18ba17625660232e2e9822011
Emilia Kura <emillia.kura@gmail.com>
2026-07-09 14:00:16 +0200

    feat: add UserService logic to set trusted user

--- src/main/java/com/audioviolencedetection/api/service/UserService.java
+++ src/main/java/com/audioviolencedetection/api/service/UserService.java
@@ -1,5 +1,11 @@
 package com.audioviolencedetection.api.service;
 
+import com.audioviolencedetection.api.dto.request.AddTrustedUserRequest;
+import com.audioviolencedetection.api.entity.User;
+import com.audioviolencedetection.api.exception.BadRequestException;
+import com.audioviolencedetection.api.exception.ItemNotFoundException;
+import com.audioviolencedetection.api.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +13,22 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void setTrustedUser(AddTrustedUserRequest request, Long currentUserId) {
+        // Check if users exist
+        User currentUser = userRepository.findById(currentUserId)
+                .orElseThrow(() -> ItemNotFoundException.createForId(User.class, currentUserId));
+
+        User trustedUser = userRepository.findByEmail(request.email())
+                .orElseThrow(() -> ItemNotFoundException.createForEmail(User.class, request.email()));
+
+        // Check if the user is not assigning himself
+        if (request.email().equalsIgnoreCase(currentUser.getEmail()))
+            throw new BadRequestException("You cannot set yourself as your own trusted user");
+
+        currentUser.setTrustedUser(trustedUser);
+        userRepository.save(currentUser);
+    }
 }
