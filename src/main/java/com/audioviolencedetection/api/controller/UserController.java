6e7802b0c8c0fde8149bf8f2d0c679afffa3b2f8
Emilia Kura <emillia.kura@gmail.com>
2026-07-09 12:51:32 +0200

    feat: prepare the controller for the patch method

--- src/main/java/com/audioviolencedetection/api/controller/UserController.java
+++ src/main/java/com/audioviolencedetection/api/controller/UserController.java
@@ -1,10 +1,24 @@
 package com.audioviolencedetection.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.audioviolencedetection.api.dto.request.AddTrustedUserRequest;
+import com.audioviolencedetection.api.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
+    @PatchMapping("/trusted-user")
+    @ResponseStatus(HttpStatus.OK)
+    public void setTrustedUser(@Valid @RequestBody AddTrustedUserRequest request,
+                               @AuthenticationPrincipal String currentUserEmail) {
+
+    }
 }
