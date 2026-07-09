2508f27f008cfb3a446ce1d74dbac8cc30fee041
Emilia Kura <emillia.kura@gmail.com>
2026-07-09 11:43:35 +0200

    fix: change BadRequestException to ResourceInUseException in AuthService for clarity and better alignment

--- src/main/java/com/audioviolencedetection/api/service/AuthService.java
+++ src/main/java/com/audioviolencedetection/api/service/AuthService.java
@@ -4,7 +4,7 @@ import com.audioviolencedetection.api.dto.request.LoginRequest;
 import com.audioviolencedetection.api.dto.request.RegisterRequest;
 import com.audioviolencedetection.api.dto.response.AuthResponse;
 import com.audioviolencedetection.api.entity.User;
-import com.audioviolencedetection.api.exception.BadRequestException;
+import com.audioviolencedetection.api.exception.ResourceInUseException;
 import com.audioviolencedetection.api.repository.UserRepository;
 import com.audioviolencedetection.api.security.model.SecurityUser;
 import com.audioviolencedetection.api.security.service.JwtService;
@@ -42,7 +42,7 @@ public class AuthService {
         // Check email availability
         String email = request.email();
         if (userRepository.existsByEmail(email))
-            throw new BadRequestException("Email is already taken");
+            throw new ResourceInUseException("Email is already taken");
 
         User user = createAndSaveUser(request);
 
