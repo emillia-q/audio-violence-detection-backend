a7feb955bf25bf6cc7ddf2f9562ab15ecb84da95
Emilia Kura <emillia.kura@gmail.com>
2026-07-10 12:02:38 +0200

    feat: add DeviceMapper class
    - It will be responsible for converting entity to response and vice versa.

---
+++ src/main/java/com/audioviolencedetection/api/mapper/DeviceMapper.java
@@ -0,0 +1,13 @@
+package com.audioviolencedetection.api.mapper;
+
+import com.audioviolencedetection.api.dto.response.DeviceListResponse;
+import com.audioviolencedetection.api.entity.Device;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeviceMapper {
+
+    public DeviceListResponse toDeviceListResponse(Device device) {
+
+    }
+}
