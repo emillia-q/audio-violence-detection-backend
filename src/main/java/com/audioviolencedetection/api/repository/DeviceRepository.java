3903c705f8807b539f486b5cd4f02e901cc42b45
Emilia Kura <emillia.kura@gmail.com>
2026-07-11 18:50:52 +0200

    refactor: made returning object optional in DeviceRepository

--- src/main/java/com/audioviolencedetection/api/repository/DeviceRepository.java
+++ src/main/java/com/audioviolencedetection/api/repository/DeviceRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DeviceRepository extends JpaRepository<Device, Long> {
 
-    Device findByMacAddress(String macAddress);
+    Optional<Device> findByMacAddress(String macAddress);
     List<Device> findAllByUserId(Long userId);
 }
