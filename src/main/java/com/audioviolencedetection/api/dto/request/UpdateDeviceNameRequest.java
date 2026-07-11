1e6d3e3ba413bd051cdaa70bfb7339c7994ea365
Emilia Kura <emillia.kura@gmail.com>
2026-07-11 15:56:44 +0200

    refactor: add messages in UpdateDeviceNameRequest record

--- src/main/java/com/audioviolencedetection/api/dto/request/UpdateDeviceNameRequest.java
+++ src/main/java/com/audioviolencedetection/api/dto/request/UpdateDeviceNameRequest.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UpdateDeviceNameRequest(
-        @NotBlank
-        @Size(max = 100)
+        @NotBlank(message = "Device name cannot be blank")
+        @Size(max = 100, message = "Device name cannot exceed 100 characters")
         String name
 ) {
 }
