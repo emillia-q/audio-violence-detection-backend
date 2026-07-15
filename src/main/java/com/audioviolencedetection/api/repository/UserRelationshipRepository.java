62507ea9ca86a6107bbd365d6993510f2647cd2b
Emilia Kura <emillia.kura@gmail.com>
2026-07-15 13:00:05 +0200

    feat: add custom query findTrustedUsersByUserId
    - It returns dto record type response list where each record contains id and email of trusted user.

--- src/main/java/com/audioviolencedetection/api/repository/UserRelationshipRepository.java
+++ src/main/java/com/audioviolencedetection/api/repository/UserRelationshipRepository.java
@@ -1,10 +1,22 @@
 package com.audioviolencedetection.api.repository;
 
+import com.audioviolencedetection.api.dto.response.TrustedUserListResponse;
 import com.audioviolencedetection.api.entity.UserRelationship;
 import com.audioviolencedetection.api.entity.UserRelationshipId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRelationshipRepository extends JpaRepository<UserRelationship, UserRelationshipId> {
+
+    @Query("select new com.audioviolencedetection.api.dto.response.TrustedUserDetailsResponse(" +
+            "r.trustedUser.id," +
+            "r.trustedUser.email" +
+            ")" +
+            "form UserRelationship r" +
+            "where r.userId = :userId")
+    List<TrustedUserListResponse> findTrustedUsersByUserId(Long userId);
 }
