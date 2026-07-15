2e36918587ecca118e5902c0f2982eeb53189285
Emilia Kura <emillia.kura@gmail.com>
2026-07-15 11:50:58 +0200

    refactor: add column name addnotaions in UserRelationshipId

--- src/main/java/com/audioviolencedetection/api/entity/UserRelationshipId.java
+++ src/main/java/com/audioviolencedetection/api/entity/UserRelationshipId.java
@@ -1,11 +1,24 @@
 package com.audioviolencedetection.api.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 
 @Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRelationshipId implements Serializable {
+
+    @Column(name = "user_id")
     private Long userId;
+
+    @Column(name = "trusted_user_id")
     private Long trustedUserId;
 }
