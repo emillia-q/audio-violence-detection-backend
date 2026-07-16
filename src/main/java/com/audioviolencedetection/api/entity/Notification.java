84e9352903bd80edebbf87c405dcaac577ce666f
Emilia Kura <emillia.kura@gmail.com>
2026-07-16 14:47:31 +0200

    feat: create Notification entity class

---
+++ src/main/java/com/audioviolencedetection/api/entity/Notification.java
@@ -0,0 +1,18 @@
+package com.audioviolencedetection.api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+
+}
