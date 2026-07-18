256fbaae7fbfcac2962550211d257d696fe1c1dc
Emilia Kura <emillia.kura@gmail.com>
2026-07-18 12:22:41 +0200

    feat: add builder annotation

--- src/main/java/com/audioviolencedetection/api/entity/Notification.java
+++ src/main/java/com/audioviolencedetection/api/entity/Notification.java
@@ -1,10 +1,7 @@
 package com.audioviolencedetection.api.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.OffsetDateTime;
 
@@ -14,6 +11,7 @@ import java.time.OffsetDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
