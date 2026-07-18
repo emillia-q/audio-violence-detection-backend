3d6750ee5c8f2bc056d219af9210d9b06663a3fd
Emilia Kura <emillia.kura@gmail.com>
2026-07-18 12:59:11 +0200

    feat: add documentation responses

--- src/main/java/com/audioviolencedetection/api/controller/AlertController.java
+++ src/main/java/com/audioviolencedetection/api/controller/AlertController.java
@@ -1,8 +1,11 @@
 package com.audioviolencedetection.api.controller;
 
 import com.audioviolencedetection.api.security.model.SecurityDevice;
+import com.audioviolencedetection.api.service.AlertService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -10,7 +13,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/alerts")
+@RequiredArgsConstructor
+@SecurityRequirement(name = "Bearer Authentication")
 public class AlertController {
+
+    private final AlertService alertService;
 
     @PostMapping
     @PreAuthorize("hasRole('DEVICE')")
@@ -18,8 +25,10 @@ public class AlertController {
     @Operation(summary = "Send alert about detected threat to database by the device")
     @ApiResponse(responseCode = "204", description = "Alert sent successfully")
     @ApiResponse(responseCode = "401", description = "Unauthorized: token possibly expired")
+    @ApiResponse(responseCode = "403", description = "Forbidden: Required role 'DEVICE' is missing")
+    @ApiResponse(responseCode = "404", description = "Device not found")
+    @ApiResponse(responseCode = "422", description = "User not assigned to the device")
     public void sendAlertToDatabase(@AuthenticationPrincipal SecurityDevice securityDevice) {
-
+        alertService.sendAlertToDatabase(securityDevice.getId());
     }
-
 }
