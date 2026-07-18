923607365fec99dbc9856a1d2ea8f1c5427ad6df
Emilia Kura <emillia.kura@gmail.com>
2026-07-18 12:12:04 +0200

    feat: add sendAlertToDatabase transactional method in AlertService
    - It creates & saves alert sent by device.

--- src/main/java/com/audioviolencedetection/api/service/AlertService.java
+++ src/main/java/com/audioviolencedetection/api/service/AlertService.java
@@ -1,6 +1,12 @@
 package com.audioviolencedetection.api.service;
 
+import com.audioviolencedetection.api.entity.Alert;
+import com.audioviolencedetection.api.entity.Device;
+import com.audioviolencedetection.api.exception.ItemNotFoundException;
 import com.audioviolencedetection.api.repository.AlertRepository;
+import com.audioviolencedetection.api.repository.DeviceRepository;
+import com.audioviolencedetection.api.repository.NotificationRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +14,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AlertService {
     private final AlertRepository alertRepository;
+    private final NotificationRepository notificationRepository;
+    private final DeviceRepository deviceRepository;
+
+    @Transactional
+    public void sendAlertToDatabase(Long deviceId) {
+        Device device = deviceRepository.findById(deviceId)
+                .orElseThrow(() -> ItemNotFoundException.createForId(Device.class, deviceId));
+
+        // Create & save alert
+        Alert alert = Alert.builder()
+                .device(device)
+                .build();
+        alertRepository.save(alert);
+
+
+    }
 }
