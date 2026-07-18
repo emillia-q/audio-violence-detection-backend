c3738e7262f04c9689ddeb307cc39d933b2ff3f4
Emilia Kura <emillia.kura@gmail.com>
2026-07-18 12:02:25 +0200

    feat: create sendAlertToDatabase method in AlertController

--- src/main/java/com/audioviolencedetection/api/controller/AlertController.java
+++ src/main/java/com/audioviolencedetection/api/controller/AlertController.java
@@ -1,9 +1,25 @@
 package com.audioviolencedetection.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.audioviolencedetection.api.security.model.SecurityDevice;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/alerts")
 public class AlertController {
+
+    @PostMapping
+    @PreAuthorize("hasRole('DEVICE')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Send alert about detected threat to database by the device")
+    @ApiResponse(responseCode = "204", description = "Alert sent successfully")
+    @ApiResponse(responseCode = "401", description = "Unauthorized: token possibly expired")
+    public void sendAlertToDatabase(@AuthenticationPrincipal SecurityDevice securityDevice) {
+
+    }
+
 }
