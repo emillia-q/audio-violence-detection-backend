c1025400f176b380373f94e506236b2b0487a8cd
Emilia Kura <emillia.kura@gmail.com>
2026-07-21 12:49:05 +0200

    feat: add toAlertListResponse method in AlertMapper
    - When name of device is null it returns "Unnamed Device" as device name.

--- src/main/java/com/audioviolencedetection/api/mapper/AlertMapper.java
+++ src/main/java/com/audioviolencedetection/api/mapper/AlertMapper.java
@@ -1,7 +1,17 @@
 package com.audioviolencedetection.api.mapper;
 
+import com.audioviolencedetection.api.dto.response.AlertListResponse;
+import com.audioviolencedetection.api.entity.Alert;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AlertMapper {
+
+    public AlertListResponse toAlertListResponse(Alert alert) {
+        return new AlertListResponse(
+                alert.getId(),
+                (alert.getDevice().getName() != null ? alert.getDevice().getName() : "Unnamed Device"),
+                alert.getCreatedAt()
+        );
+    }
 }
