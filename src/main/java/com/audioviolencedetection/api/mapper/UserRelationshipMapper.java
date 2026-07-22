897172987c965c43541cef74e4ca8561c129e117
Emilia Kura <emillia.kura@gmail.com>
2026-07-22 15:06:20 +0200

    feat: add UserRelationshipMapper with toProtectedUserListResponse
    - It maps projection to response dto.

--- src/main/java/com/audioviolencedetection/api/mapper/UserRelationshipMapper.java
+++ src/main/java/com/audioviolencedetection/api/mapper/UserRelationshipMapper.java
@@ -1,6 +1,8 @@
 package com.audioviolencedetection.api.mapper;
 
+import com.audioviolencedetection.api.dto.response.ProtectedUserListResponse;
 import com.audioviolencedetection.api.dto.response.TrustedUserListResponse;
+import com.audioviolencedetection.api.repository.projection.ProtectedUserListProjection;
 import com.audioviolencedetection.api.repository.projection.TrustedUserListProjection;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +13,13 @@ public class UserRelationshipMapper {
         return new TrustedUserListResponse(
                 proj.getTrustedUserId(),
                 proj.getTrustedUserNickname()
+        );
+    }
+
+    public ProtectedUserListResponse toProtectedUserListResponse(ProtectedUserListProjection proj) {
+        return new ProtectedUserListResponse(
+                proj.getProtectedUserId(),
+                proj.getProtectedUserNickname()
         );
     }
 }
