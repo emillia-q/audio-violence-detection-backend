949803f52970ed57342578b8b0333eac2f9cbb82
Emilia Kura <emillia.kura@gmail.com>
2026-07-22 15:00:00 +0200

    fix: findTrustedUsersByUserId query
    - Deleted hardcoded path  to list dto instead use projection.

--- src/main/java/com/audioviolencedetection/api/repository/UserRelationshipRepository.java
+++ src/main/java/com/audioviolencedetection/api/repository/UserRelationshipRepository.java
@@ -1,9 +1,9 @@
 package com.audioviolencedetection.api.repository;
 
 import com.audioviolencedetection.api.dto.response.ProtectedUserListResponse;
-import com.audioviolencedetection.api.dto.response.TrustedUserListResponse;
 import com.audioviolencedetection.api.entity.UserRelationship;
 import com.audioviolencedetection.api.entity.UserRelationshipId;
+import com.audioviolencedetection.api.repository.projection.TrustedUserListProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,13 +13,10 @@ import java.util.List;
 @Repository
 public interface UserRelationshipRepository extends JpaRepository<UserRelationship, UserRelationshipId> {
 
-    @Query("select new com.audioviolencedetection.api.dto.response.TrustedUserListResponse(" +
-            "r.trustedUser.id, " +
-            "r.nicknameForTrusted" +
-            ") " +
+    @Query("select r.trustedUser.id as trustedUserId, r.nicknameForTrusted as trustedUserNickname " +
             "from UserRelationship r " +
             "where r.user.id = :userId")
-    List<TrustedUserListResponse> findTrustedUsersByUserId(Long userId);
+    List<TrustedUserListProjection> findTrustedUsersByUserId(Long userId);
 
     @Query("select new com.audioviolencedetection.api.dto.response.ProtectedUserListResponse(" +
             "r.user.id, " +
