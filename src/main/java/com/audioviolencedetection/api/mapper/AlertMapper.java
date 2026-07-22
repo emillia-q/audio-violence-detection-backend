ba283c82767c3c4ca10ba598e15a40df7f7ac2a8
Emilia Kura <emillia.kura@gmail.com>
2026-07-22 14:21:54 +0200

    feat: add toProtectedUsersAlertListResponse method in AlertMapper
    - Converts data retrieved from database to projection interface to dto response.

--- src/main/java/com/audioviolencedetection/api/mapper/AlertMapper.java
+++ src/main/java/com/audioviolencedetection/api/mapper/AlertMapper.java
@@ -1,7 +1,9 @@
 package com.audioviolencedetection.api.mapper;
 
 import com.audioviolencedetection.api.dto.response.AlertListResponse;
+import com.audioviolencedetection.api.dto.response.AlertProtectedUsersListResponse;
 import com.audioviolencedetection.api.entity.Alert;
+import com.audioviolencedetection.api.repository.projection.AlertProtectedUserProjection;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +14,16 @@ public class AlertMapper {
                 alert.getId(),
                 (alert.getDevice().getName() != null ? alert.getDevice().getName() : "Unnamed Device"),
                 alert.getCreatedAt()
+        );
+    }
+
+    public AlertProtectedUsersListResponse toProtectedUsersAlertListResponse(AlertProtectedUserProjection proj) {
+        return new AlertProtectedUsersListResponse(
+                proj.getAlertId(),
+                proj.getProtectedUserId(),
+                proj.getProtectedUserNickname(),
+                (proj.getDeviceName() != null ? proj.getDeviceName() : "Unnamed Device"),
+                proj.getCreatedAt()
         );
     }
 }
