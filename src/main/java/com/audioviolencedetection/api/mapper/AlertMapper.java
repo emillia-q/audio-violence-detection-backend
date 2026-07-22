6502ea7437d112b66fb6ff603d4df37322a5ee12
Emilia Kura <emillia.kura@gmail.com>
2026-07-22 14:38:13 +0200

    fix: align toAlertListResponse method in AlertMapper to received type

--- src/main/java/com/audioviolencedetection/api/mapper/AlertMapper.java
+++ src/main/java/com/audioviolencedetection/api/mapper/AlertMapper.java
@@ -3,17 +3,18 @@ package com.audioviolencedetection.api.mapper;
 import com.audioviolencedetection.api.dto.response.AlertListResponse;
 import com.audioviolencedetection.api.dto.response.AlertProtectedUsersListResponse;
 import com.audioviolencedetection.api.entity.Alert;
+import com.audioviolencedetection.api.repository.projection.AlertProjection;
 import com.audioviolencedetection.api.repository.projection.AlertProtectedUserProjection;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AlertMapper {
 
-    public AlertListResponse toAlertListResponse(Alert alert) {
+    public AlertListResponse toAlertListResponse(AlertProjection proj) {
         return new AlertListResponse(
-                alert.getId(),
-                (alert.getDevice().getName() != null ? alert.getDevice().getName() : "Unnamed Device"),
-                alert.getCreatedAt()
+                proj.getId(),
+                (proj.getDeviceName() != null ? proj.getDeviceName() : "Unnamed Device"),
+                proj.getCreatedAt()
         );
     }
 
