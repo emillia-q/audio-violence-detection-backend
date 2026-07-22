c377e8a56a4714f1235d253f4c1075156d3a7452
Emilia Kura <emillia.kura@gmail.com>
2026-07-22 14:09:36 +0200

    feat: add findProtectedUsersAlerts query in AlertRepository
    - It finds all alerts for all protected users for trusted user.

--- src/main/java/com/audioviolencedetection/api/repository/AlertRepository.java
+++ src/main/java/com/audioviolencedetection/api/repository/AlertRepository.java
@@ -1,6 +1,7 @@
 package com.audioviolencedetection.api.repository;
 
 import com.audioviolencedetection.api.entity.Alert;
+import com.audioviolencedetection.api.repository.projection.AlertProtectedUserProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -15,4 +16,13 @@ public interface AlertRepository extends JpaRepository<Alert, Long> {
             "where a.device.user.id = :userId " +
             "order by a.createdAt desc")
     List<Alert> findAllByUserId(Long userId);
+
+    @Query("select a.id as alertId, r.user.id as protectedUserId, r.nicknameForSupervised as protectedUserNickname, " +
+            "d.name as deviceName, a.createdAt as createdAt " +
+            "from Alert a " +
+            "join a.device d " +
+            "join UserRelationship r on r.user.id = d.user.id " +
+            "where r.trustedUser.id = :userId " +
+            "order by a.createdAt desc")
+    List<AlertProtectedUserProjection> findProtectedUsersAlerts(Long userId);
 }
