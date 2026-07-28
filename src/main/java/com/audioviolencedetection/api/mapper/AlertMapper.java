327ff572288653cab281b6b3714ec983ee2078da
Emilia Kura <emillia.kura@gmail.com>
2026-07-28 13:32:12 +0200

    refactor: switch projection field from getProtectedUserNickname to protectedUserDisplayName

--- src/main/java/com/audioviolencedetection/api/mapper/AlertMapper.java
+++ src/main/java/com/audioviolencedetection/api/mapper/AlertMapper.java
@@ -21,7 +21,7 @@ public class AlertMapper {
         return new AlertProtectedUsersListResponse(
                 proj.getAlertId(),
                 proj.getProtectedUserId(),
-                proj.getProtectedUserNickname(),
+                proj.protectedUserDisplayName(),
                 (proj.getDeviceName() != null ? proj.getDeviceName() : "Unnamed Device"),
                 proj.getCreatedAt()
         );
