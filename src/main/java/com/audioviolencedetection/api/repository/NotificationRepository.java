e73ee361970f9e2631170765e4f4822f3a16a468
Emilia Kura <emillia.kura@gmail.com>
2026-08-08 11:59:21 +0200

    feat: add custom query
    - Add query that deletes all protected user notifications from all his devices for trusted user.

--- src/main/java/com/audioviolencedetection/api/repository/NotificationRepository.java
+++ src/main/java/com/audioviolencedetection/api/repository/NotificationRepository.java
@@ -2,6 +2,14 @@ package com.audioviolencedetection.api.repository;
 
 import com.audioviolencedetection.api.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    @Modifying
+    @Query("delete from Notification n " +
+            "where n.trustedUser.id = :trustedUserId " +
+            "and n.alert.device.user.id = :protectedUserId")
+    void deleteByTrustedUserAndDeviceOwner(Long protectedUserId, Long trustedUserId);
 }
