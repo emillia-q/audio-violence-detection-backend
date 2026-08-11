52e24cbb1546a54581d03efb0ad4b4c8f78fee33
Emilia Kura <emillia.kura@gmail.com>
2026-08-11 12:09:17 +0200

    feat: add NotificationController

---
+++ src/main/java/com/audioviolencedetection/api/controller/NotificationController.java
@@ -0,0 +1,15 @@
+package com.audioviolencedetection.api.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/notifications")
+@RequiredArgsConstructor
+@SecurityRequirement(name = "Bearer Authentication")
+@PreAuthorize("hasRole('USER')")
+public class NotificationController {
+}
