2fc2fb9b5495e6332c6bf7538070db8bcfc8ac77
Emilia Kura <emillia.kura@gmail.com>
2026-08-18 10:51:31 +0200

    feat: add getProtectedUsersNotifications method

--- src/main/java/com/audioviolencedetection/api/service/NotificationService.java
+++ src/main/java/com/audioviolencedetection/api/service/NotificationService.java
@@ -1,8 +1,23 @@
 package com.audioviolencedetection.api.service;
 
+import com.audioviolencedetection.api.dto.response.NotificationListResponse;
+import com.audioviolencedetection.api.mapper.NotificationMapper;
+import com.audioviolencedetection.api.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class NotificationService {
 
+    private final NotificationRepository notificationRepository;
+    private final NotificationMapper notificationMapper;
+
+    public List<NotificationListResponse> getProtectedUsersNotifications(Long trustedUserId) {
+        return notificationRepository.findProtectedUsersAlerts(trustedUserId).stream()
+                .map(notificationMapper::toNotificationListResponse)
+                .toList();
+    }
 }
