963c3c38d44a80d5bf0f06cc2137d3f4b4727dc3
Emilia Kura <emillia.kura@gmail.com>
2026-08-18 11:54:23 +0200

    feat: add toggleNotificationStatus method

--- src/main/java/com/audioviolencedetection/api/service/NotificationService.java
+++ src/main/java/com/audioviolencedetection/api/service/NotificationService.java
@@ -1,6 +1,8 @@
 package com.audioviolencedetection.api.service;
 
 import com.audioviolencedetection.api.dto.response.NotificationListResponse;
+import com.audioviolencedetection.api.entity.Notification;
+import com.audioviolencedetection.api.exception.ItemNotFoundException;
 import com.audioviolencedetection.api.mapper.NotificationMapper;
 import com.audioviolencedetection.api.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,5 +21,12 @@ public class NotificationService {
         return notificationRepository.findProtectedUsersAlerts(trustedUserId).stream()
                 .map(notificationMapper::toNotificationListResponse)
                 .toList();
+    }
+
+    public void toggleNotificationStatus(Long notificationId) {
+        Notification notification = notificationRepository.findById(notificationId)
+                .orElseThrow(() -> ItemNotFoundException.createForId(Notification.class, notificationId));
+
+        notification.setRead(!notification.isRead());
     }
 }
