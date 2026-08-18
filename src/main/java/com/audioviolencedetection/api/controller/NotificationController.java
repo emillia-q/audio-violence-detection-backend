5ecf7efb53f4ca6e6dc0d3b89b01e036481ab24e
Emilia Kura <emillia.kura@gmail.com>
2026-08-18 10:53:16 +0200

    feat: add getProtectedUsersNotifications endpoint

--- src/main/java/com/audioviolencedetection/api/controller/NotificationController.java
+++ src/main/java/com/audioviolencedetection/api/controller/NotificationController.java
@@ -1,10 +1,20 @@
 package com.audioviolencedetection.api.controller;
 
+import com.audioviolencedetection.api.dto.response.NotificationListResponse;
+import com.audioviolencedetection.api.security.model.SecurityUser;
+import com.audioviolencedetection.api.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/notifications")
@@ -13,5 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 @PreAuthorize("hasRole('USER')")
 public class NotificationController {
 
+    private final NotificationService notificationService;
 
+    @GetMapping
+    @Operation(summary = "Get list of all protected users notifications")
+    @ApiResponse(responseCode = "200", description = "Return list of notifications")
+    @ApiResponse(responseCode = "204", description = "No notifications")
+    public ResponseEntity<List<NotificationListResponse>> getProtectedUsersNotifications(
+            @AuthenticationPrincipal SecurityUser securityUser) {
+        List<NotificationListResponse> notifications = notificationService.getProtectedUsersNotifications(securityUser.getId());
+
+        if (notifications.isEmpty())
+            return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok(notifications);
+    }
 }
