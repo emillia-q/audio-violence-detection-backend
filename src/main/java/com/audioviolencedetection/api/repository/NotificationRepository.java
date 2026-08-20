1bca703517de97149da9bc53e94b04feacabfb5f
Emilia Kura <emillia.kura@gmail.com>
2026-08-20 10:46:13 +0200

    feat: add Pageable interface

--- src/main/java/com/audioviolencedetection/api/repository/NotificationRepository.java
+++ src/main/java/com/audioviolencedetection/api/repository/NotificationRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
