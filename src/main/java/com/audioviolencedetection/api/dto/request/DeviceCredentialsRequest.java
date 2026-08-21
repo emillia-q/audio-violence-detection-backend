ac7d8a48290617d8e1bf59ee608149606ea7e894
Emilia Kura <emillia.kura@gmail.com>
2026-08-21 17:34:57 +0200

    refactor: rename dto request

--- src/main/java/com/audioviolencedetection/api/dto/request/DeviceCredentialsRequest.java
+++ src/main/java/com/audioviolencedetection/api/dto/request/DeviceCredentialsRequest.java
@@ -3,7 +3,7 @@ package com.audioviolencedetection.api.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
-public record DevicePairRequest(
+public record DeviceCredentialsRequest(
         @NotBlank(message = "MAC address is required")
         @Pattern(regexp = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", message = "Invalid MAC address format")
         String macAddress,
