9291cd8335b9d1bdf6cc9c1079e86b027cf206d9
Emilia Kura <emillia.kura@gmail.com>
2026-08-27 12:23:55 +0200

    feat: add isActivated flag
    - Check whether the device is activated otherwise throw 422 status response code.

--- src/main/java/com/audioviolencedetection/api/service/AlertService.java
+++ src/main/java/com/audioviolencedetection/api/service/AlertService.java
@@ -57,7 +57,7 @@ public class AlertService {
                 .orElseThrow(() -> ItemNotFoundException.createForId(Device.class, deviceId));
 
         User protectedUser = device.getUser();
-        if (protectedUser == null)
+        if (protectedUser == null || !device.getIsActivated())
             throw new UnprocessableEntityException("Device must be activated and paired with a user before sending alerts");
 
         // Create & save alert
